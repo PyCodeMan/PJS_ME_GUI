3dc6059af6eaff7a9e4d3c217e94ddf08e8bd4af
PyCodeMan <140621328+PyCodeMan@users.noreply.github.com>
2023-11-20 07:14:47 +0800

    Create texts_templates.py

---
+++ texts_templates.py
@@ -0,0 +1,24 @@
+question_repeat_time_text = 'С какой периодичностью должны обновляться тексты заказов? Укажите количество минут'
+question_repeat_ones_text = 'Сколько раз должно произойти обновление?'
+hello_text = 'Добро пожаловать в автоматизированную систему поиска заказов на фрилансе!'
+refresh_manually_text = 'Однократно обновить тексты заказов'
+refresh_automatically_text = 'Автоматически обновлять тексты заказов'
+link_button_text = 'Открыть текст заказа в браузере'
+alarm_text = 'Вы ввели не число'
+
+text_config = {
+    'borderwidth' : 2,
+    'font' : ('Verdana', 10),
+    'relief' : 'groove',
+    'justify' : 'left'
+}
+
+colors_config = {
+    'background' : '#808080',
+    'foreground' : '#FFFFFF'
+}
+
+url = 'https://freelance.ru/project/search/pro?c=&c%5B%5D=116&c%5B%5D=4&q=&m=or&e=&a=0&a=1&v=0&f=&t=&o=0&o=1&b='
+base_url = 'https://freelance.ru/'
+tag = 'a'
+class_name = 'description'
